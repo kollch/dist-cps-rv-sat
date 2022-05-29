e20dadc5fbb847d60afb8a4f81a5e3b693db35b1
Charles Koll <charles@kollonline.com>
2022-05-29 11:25:00 -0700

    Build Z3 formula expression

--- src/RVSAT.jl
+++ src/RVSAT.jl
@@ -1,6 +1,7 @@
 module RVSAT
 
 using Match
+using Z3
 
 function strtoexpr(s)
     # Ensure that Julia can interpret the formula correctly
@@ -50,11 +51,44 @@ function tbounds(signals)
     zeros(n), maximum.(times)
 end
 
-function validsigs(signals)
+function initsigs(signals)
     if length(signals) % 2 ≠ 0
         error("missing signal for provided timesteps")
     end
-    map(x -> convert.(AbstractFloat, x), signals)
+    #map(x -> convert.(AbstractFloat, x), signals)
+    map(x -> (collect ∘ zip)(x...), Iterators.partition(signals, 2))
+end
+
+function z3example()
+    ctx = Context()
+    s = Solver(ctx, "QF_LRA")
+    t = real_const(ctx, "t")
+    #x = func(ctx, "x", real_sort(ctx), real_sort(ctx))
+    x = real_const(ctx, "x")
+    add(s, and(t ≥ 0, t ≤ 5))
+    #add(s, x(t) == t - 3)
+    add(s, x == t - 3)
+    add(s, x ≥ 0)
+    res = check(s)
+    m = get_model(s)
+    for (k, v) in consts(m)
+        println("$k = $v")
+    end
+end
+
+function z3formula(ϕ)
+    @match ϕ begin
+        Expr(:call, [:!, x::Expr])             => z3formula(negate(x))
+        Expr(:call, [r::Symbol, x, c::Number]) => Expr(:call, r, x, c) |> eval
+        Expr(:&&, [x::Expr, y::Expr])          => and(z3formula(x), z3formula(y))
+        Expr(:||, [x::Expr, y::Expr])          => or(z3formula(x), z3formula(y))
+        _ => error("invalid formula: ", ϕ)
+    end
+end
+
+function signalsat(ϕ, ε, signals...)
+    signals = initsigs(signals)
+    formula = (z3formula ∘ strtoexpr)(ϕ)
 end
 
 end # module
